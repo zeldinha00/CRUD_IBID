91076df97c3e78d173792d33810b15d16b79812c
Roger Oliveira Silva <zeldinhamtx@gmail.com>
2023-09-27 02:00:29 -0300

    initial commit

---
+++ Project.IBID.CRUD/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Nome { get; set; }
+}
+
+public class ProductManager
+{
+    private List<Product> products = new List<Product>();
+    private int nextId = 1;
+
+    public void AddProduct(Product product)
+    {
+        product.ID = nextId++;
+        products.Add(product);
+    }
+
+    public void RemoveProduct(int productId)
+    {
+        Product productToRemove = products.FirstOrDefault(p => p.ID == productId);
+        if (productToRemove != null)
+        {
+            products.Remove(productToRemove);
+        }
+    }
+
+    public void UpdateProduct(int productId, string newName)
+    {
+        Product productToUpdate = products.FirstOrDefault(p => p.ID == productId);
+        if (productToUpdate != null)
+        {
+            productToUpdate.Nome = newName;
+        }
+    }
+
+    public List<Product> GetProducts()
+    {
+        return products;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Seja bem-vindo ao CRUD IBID\n");
+
+        ProductManager productManager = new ProductManager();
+
+        while (true)
+        {
+            Console.WriteLine("Opções:");
+            Console.WriteLine("1 - Adicionar Produto");
+            Console.WriteLine("2 - Remover Produto");
+            Console.WriteLine("3 - Editar Nome do Produto");
+            Console.WriteLine("4 - Visualizar Todos os Produtos");
+            Console.WriteLine("5 - Sair");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Nome do Produto: ");
+                    string nome = Console.ReadLine();
+                    Product newProduct = new Product { Nome = nome };
+                    productManager.AddProduct(newProduct);
+                    Console.WriteLine("Produto adicionado com sucesso.");
+                    break;
+
+                case 2:
+                    Console.Write("ID do Produto a ser removido: ");
+                    int productIdToRemove = int.Parse(Console.ReadLine());
+                    productManager.RemoveProduct(productIdToRemove);
+                    Console.WriteLine("Produto removido com sucesso.");
+                    break;
+
+                case 3:
+                    Console.Write("ID do Produto a ser editado: ");
+                    int productIdToEdit = int.Parse(Console.ReadLine());
+                    Console.Write("Novo Nome: ");
+                    string newName = Console.ReadLine();
+                    productManager.UpdateProduct(productIdToEdit, newName);
+                    Console.WriteLine("Nome do produto atualizado com sucesso.");
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Lista de Produtos:");
+                    Console.WriteLine("ID\tNome");
+
+                    foreach (Product product in productManager.GetProducts())
+                    {
+                        Console.WriteLine($"{product.ID}\t{product.Nome}");
+                    }
+
+                    Console.WriteLine();
+                    break;
+
+                case 5:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# CRUD_IBID
- Desafio - Boocamp 4ª edição
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=87CEFA&height=120&section=header"/>
+
+#  Desafio - Boocamp 4ª edição
+
+### Abrindo o Sistema
+
+- Basta clicar em Run and Debug e depois em Run Debugging (F5)
+  
+![image](https://github.com/zeldinha00/CRUD_IBID/assets/14182590/25bdcc0d-18a1-470e-aaf7-bfd066d993b6)
+
+- Com isso vai abrir o Terminal
+
+### PROGRAMA RODANDO BASTAR USAR AS FUNCIONALIDADES
+
+![Apex_1695790326824](https://github.com/zeldinha00/zeldinha00/assets/14182590/5c7e03e4-d9a7-413b-8c52-0352b0cae49d)
+
+
+<div align="center">  
+<a href="https://www.linkedin.com/in/roger0liveira/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"</a>
+<a href="https://www.instagram.com/zeldinha00/" target="_blank"><img src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white"</a>
+</div>
+
+
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=87CEFA&height=120&section=footer"/>
+
+
+
+
+
